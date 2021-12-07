81a38476b6a665847257dffcb4601e465dc07c15
Joe Reed <jreed@fastly.com>
2021-12-07 11:08:56 +0000

    Day Five

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
-[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -97,9 +97,9 @@ Both parts: 12.08ms
 
 ```
 Day 05
-Time part 1: 40.26ms
-Time part 2: -
-Both parts: 40.26ms
+Time part 1: 38.68ms
+Time part 2: 187.79ms
+Both parts: 226.47ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 9/50
-Total time: 62.769999999999996ms
+Total stars: 10/50
+Total time: 248.98ms
 ```
 
 <!--/RESULTS-->
--- src/day05/index.js
+++ src/day05/index.js
@@ -2,9 +2,7 @@ import run from "aocrunner"
 
 const parseInput = (rawInput) => rawInput.split("\n").map(row => row.split(" -> "))
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
-
+const plotLines = (input, { includeDiagonals } = {}) => {
   const board = new Map()
 
   const drawLine = (axis, lineStart, lineEnd, isX = false) => {
@@ -21,23 +19,52 @@ const part1 = (rawInput) => {
     }
   }
 
+  const drawDiagonal = ([x1, y1], [x2, y2]) => {
+    let x = x1
+    let y = y1
+
+    const xCounter = Math.sign(x2 - x1)
+    const yCounter = Math.sign(y2 - y1)
+
+    while (x !== x2 + xCounter && y !== y2 + yCounter) {
+      const key = `${x}-${y}`
+
+      const value = board.get(key) ? board.get(key) + 1 : 1
+      board.set(key, value)
+
+      x += xCounter
+      y += yCounter
+    }
+  }
+
   let i = 0
 
   while (i < input.length) {
     const [startCoords, endCoords] = input[i]
 
-    const [x1, y1] = startCoords.split(",")
-    const [x2, y2] = endCoords.split(",")
+    const [x1, y1] = startCoords.split(",").map(n => parseInt(n, 10))
+    const [x2, y2] = endCoords.split(",").map(n => parseInt(n, 10))
 
     if (x1 === x2) {
-      drawLine(parseInt(x1, 10), parseInt(y1, 10), parseInt(y2, 10), true)
+      drawLine(x1, y1, y2, true)
     }
     else if (y1 === y2) {
-      drawLine(parseInt(y1, 10), parseInt(x1, 10), parseInt(x2, 10))
+      drawLine(y1, x1, x2)
+    }
+    else if (includeDiagonals) {
+      drawDiagonal([x1, y1], [x2, y2])
     }
 
     i += 1
   }
+
+  return board
+}
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput)
+
+  const board = plotLines(input)
 
   return [...board.values()].filter(v => v > 1).length
 }
@@ -45,7 +72,11 @@ const part1 = (rawInput) => {
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
 
-  return
+  const board = plotLines(input, { includeDiagonals: true })
+
+  console.log([...board.entries()])
+
+  return [...board.values()].filter(v => v > 1).length
 }
 
 run({
@@ -70,7 +101,20 @@ run({
   },
   part2: {
     tests: [
-      // { input: ``, expected: "" },
+      {
+        input: `
+      0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+      `, expected: 12
+      },
     ],
     solution: part2,
   },
